219ddb30bcefc6b58c6a991bc6b25d1179591fdb
stolomeo <tolomeosebastian@gmail.com>
2022-05-23 14:43:52 -0400

    Added login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,10 +17,20 @@ const app = initializeApp(firebaseConfig);
 
 export default function Login() {
   return (
-    <Form labelCol={{ span: 4 }} wrapperCol={{ span: 16 }}>
-      <Form.Item label="Email" name="email">
-        <Input />
-      </Form.Item>
-    </Form>
+    <section style={{ padding: "2em" }}>
+      <Form labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+        <Form.Item label="Email" name="email">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Password" name="password">
+          <Input.Password />
+        </Form.Item>
+        <Form.Item wrapperCol={{ span: 16, offset: 8 }}>
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
+    </section>
   );
 }
